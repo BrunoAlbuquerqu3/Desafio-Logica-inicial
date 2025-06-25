1aa69e666b59c8b209a07743fc66bbde1c6e12d6
Bruno Albuquerque <bruno30break@gmail.com>
2025-06-25 01:11:06 -0300

    cadastro de produtos no carrinho

---
+++ AprendendoFuncao.js
@@ -0,0 +1,12 @@
+torrar()
+injetarPao()
+
+
+function torrar(){
+    console.log("Torrando pão")
+}
+
+function injetarPao(){
+    console.log("Preparando para Injetar pão")
+    console.log("Finalizado")
+}
